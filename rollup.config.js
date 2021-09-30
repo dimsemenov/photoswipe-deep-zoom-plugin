d530e6657198a9f5571980327f2c8c948670d506
Dmitry Semenov <diiiimaaaa@gmail.com>
2021-09-30 09:32:00 +0300

    Updates

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-import { terser } from "rollup-plugin-terser";
- 
-export default {
-  input: "src/index.js",
-  output: [
-    { file: "photoswipe-deep-zoom-plugin.esm.js", format: "esm" },
-    { file: "photoswipe-deep-zoom-plugin.esm.min.js", format: "esm", plugins: [ terser()] },
-  ],
-};
