0386c86475478d304b0b01680ebf513a0d324ab6
Dmitry Semenov <diiiimaaaa@gmail.com>
2022-01-25 20:02:43 +0200

    lib update

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,16 @@
 import { terser } from "rollup-plugin-terser";
- 
+
+const banner = `/**
+* PhotoSwipe Deep Zoom plugin
+* v1.1.0
+* by Dmytro Semenov
+* https://github.com/dimsemenov/photoswipe-deep-zoom-plugin
+*/`;
+
 export default {
   input: "src/index.js",
   output: [
-    { file: "dist/photoswipe-deep-zoom-plugin.esm.js", format: "esm" },
-    { file: "dist/photoswipe-deep-zoom-plugin.esm.min.js", format: "esm", plugins: [ terser()] },
+    { file: "dist/photoswipe-deep-zoom-plugin.esm.js", format: "esm", banner },
+    { file: "dist/photoswipe-deep-zoom-plugin.esm.min.js", format: "esm", plugins: [ terser()], banner },
   ],
 };
